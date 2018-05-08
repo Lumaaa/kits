4bd51e466f441a479eecd7f758c61bc968c93e12
Norbert Nagy <nagy.nbt@gmail.com>
2018-05-08 21:23:54 +0200

    Initial commit
    Project set up + Database linking

---
+++ kits/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kits.Startup))]
+namespace kits
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
