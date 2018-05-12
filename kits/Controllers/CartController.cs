7270f2ca27b499f5888137f3283633d8f3c75ccd
Norbert Nagy <nagy.nbt@gmail.com>
2018-05-12 17:17:40 +0200

    Work on cart

---
+++ kits/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using kits.Models;
+using kits.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kits.Controllers
+{
+    public class CartController : Controller
+    {
+        private kitsEntities db = new kitsEntities();
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            product product = db.products.Find(productId);
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { controller = returnUrl.Substring(1) });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null) { cart = new Cart(); Session["Cart"] = cart; }
+            return cart;
+        }
+
+    }
+}
