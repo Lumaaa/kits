f29ad4a1e125b6b48e479e028068feffe684bf1a
Norbert Nagy <nagy.nbt@gmail.com>
2018-05-15 22:43:14 +0200

    Webshop sort by category

--- kits/Controllers/productsController.cs
+++ kits/Controllers/productsController.cs
@@ -13,13 +13,27 @@ namespace kits.Controllers
     public class productsController : Controller
     {
         private kitsEntities db = new kitsEntities();
+        private IEnumerable<product> products = new List<product>();
+        private List<SelectListItem> categories = new List<SelectListItem>();
 
         // GET: products
         public ActionResult Index()
         {
             var products = db.products.Include(p => p.brand).Include(p => p.category);
-            //categories to do
-            return View(products.ToList());
+            foreach (var category in db.categories)
+            {
+                categories.Add(new SelectListItem
+                {
+                    Text = category.name,
+                    Value = category.category_ID.ToString()
+                });
+            }
+            products = db.products;
+
+            ViewBag.CategoryId = categories;
+            ViewBag.Products = products;
+
+            return View();
         }
 
         // GET: products/Details/5
@@ -45,92 +59,28 @@ namespace kits.Controllers
             return View(product);
         }
 
-        // GET: products/Create
-        public ActionResult Create()
+        [HttpPost]
+        public ActionResult Index(int? CategoryId)
         {
-            ViewBag.brand_ID = new SelectList(db.brands, "brand_ID", "name");
-            ViewBag.category_ID = new SelectList(db.categories, "category_ID", "name");
+            foreach (var category in db.categories)
+            {
+                categories.Add(new SelectListItem
+                {
+                    Text = category.name,
+                    Value = category.category_ID.ToString()
+                });
+            }
+
+            if (CategoryId == null) {
+                products = db.products;
+            } else {
+                products = db.products.Where(r => r.category_ID == CategoryId);
+            }
+
+            ViewBag.CategoryId = categories;
+            ViewBag.Products = products;
+
             return View();
-        }
-
-        // POST: products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "product_ID,product_name,brand_ID,category_ID,product_description,size_ID,product_price")] product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.brand_ID = new SelectList(db.brands, "brand_ID", "name", product.brand_ID);
-            ViewBag.category_ID = new SelectList(db.categories, "category_ID", "name", product.category_ID);
-            return View(product);
-        }
-
-        // GET: products/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            product product = db.products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.brand_ID = new SelectList(db.brands, "brand_ID", "name", product.brand_ID);
-            ViewBag.category_ID = new SelectList(db.categories, "category_ID", "name", product.category_ID);
-            return View(product);
-        }
-
-        // POST: products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "product_ID,product_name,brand_ID,category_ID,product_description,size_ID,product_price")] product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.brand_ID = new SelectList(db.brands, "brand_ID", "name", product.brand_ID);
-            ViewBag.category_ID = new SelectList(db.categories, "category_ID", "name", product.category_ID);
-            return View(product);
-        }
-
-        // GET: products/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            product product = db.products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            product product = db.products.Find(id);
-            db.products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
